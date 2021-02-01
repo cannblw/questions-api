773288038dec3d08e753e828f48970f780f7b869
cannblw <edgar.95.12@gmail.com>
2021-02-01 18:16:05 +0100

    Rename IndexController to QuestionsController

---
+++ src/Controller/QuestionsController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class QuestionsController extends AbstractController
+{
+    /**
+     * @Route("/questions", methods={"GET"})
+     */
+    public function getQuestions(): Response
+    {
+        return $this->json([
+            'message' => 'Get questions',
+        ]);
+    } 
+
+    /**
+     * @Route("/questions", methods={"POST"})
+     */
+    public function createQuestion(): Response
+    {
+        return $this->json([
+            'message' => 'Create question',
+        ]);
+    } 
+}
