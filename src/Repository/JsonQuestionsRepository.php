fca6678062f59d9894f16e9adb3d5da4b55a2f6f
cannblw <edgar.95.12@gmail.com>
2021-02-01 19:16:27 +0100

    Create JSON questions repository

---
+++ src/Repository/JsonQuestionsRepository.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Repository;
+
+use App\Domain\Question;
+
+const DATA_SOURCE_PATH = '../../dataSources/questions.json';
+
+class JsonQuestionsRepository implements QuestionsRepository
+{
+    private $questions = [];
+
+    public function getQuestions()
+    {
+        return $this->questions;
+    }
+
+    public function addQuestion(Question $question)
+    {
+        array_push($this->questions, $question);
+    }
+
+    public function load()
+    {
+        $content = file_get_contents(DATA_SOURCE_PATH);
+        $decoded_content = json_decode($content);
+
+        $this->questions = $decoded_content;
+    }
+}
