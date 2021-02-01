e03eef95d980637440436578c6212c345356c17b
cannblw <edgar.95.12@gmail.com>
2021-02-01 19:37:55 +0100

    Create get endpoint

--- src/Controller/QuestionsController.php
+++ src/Controller/QuestionsController.php
@@ -2,20 +2,24 @@
 
 namespace App\Controller;
 
+use App\Repository\QuestionsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class QuestionsController extends AbstractController
 {
+    public function __construct(QuestionsRepository $repository)
+    {
+        $repository->load();
+    }
+
     /**
      * @Route("/questions", methods={"GET"})
      */
-    public function getQuestions(): Response
+    public function getQuestions(QuestionsRepository $repository): Response
     {
-        return $this->json([
-            'message' => 'Get questions',
-        ]);
+        return $this->json($repository->getQuestions());
     }
 
     /**
