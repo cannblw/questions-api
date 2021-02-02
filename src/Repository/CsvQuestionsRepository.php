0fea5f94ed561f7f1ef24500991ad5d2ab680a9d
cannblw <edgar.95.12@gmail.com>
2021-02-02 11:53:22 +0100

    Fix CSV parser

--- src/Repository/CsvQuestionsRepository.php
+++ src/Repository/CsvQuestionsRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repository;
 
+use App\Domain\Choice;
 use App\Domain\Question;
 
 class CsvQuestionsRepository implements QuestionsRepository
@@ -29,19 +30,28 @@ class CsvQuestionsRepository implements QuestionsRepository
         $fp = fopen($this->data_source_path, 'r');
 
         // Ignore column titles
-        fgetcsv($fp, '1024', ',');
+        fgetcsv($fp);
 
         $json = [];
 
-        while ($row = fgetcsv($fp, '1024', ',')) {
+        while ($row = fgetcsv($fp)) {
             $question = new Question();
+
+            $choice1 = new Choice();
+            $choice1->text = $row[2];
+
+            $choice2 = new Choice();
+            $choice2->text = $row[3];
+
+            $choice3 = new Choice();
+            $choice3->text = $row[4];
 
             $question->text = $row[0];
             $question->createdAt = $row[1];
             $question->choices = [
-                ['text' => $row[2]],
-                ['text' => $row[3]],
-                ['text' => $row[4]],
+                $choice1,
+                $choice2,
+                $choice3,
             ];
 
             array_push($json, $question);
