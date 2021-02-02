8bb0cef34b7f87764c9181341cbefadde2d69238
cannblw <edgar.95.12@gmail.com>
2021-02-02 10:02:15 +0100

    Update JSON repository to persist data

--- src/Repository/JsonQuestionsRepository.php
+++ src/Repository/JsonQuestionsRepository.php
@@ -7,28 +7,23 @@ use App\Domain\Question;
 class JsonQuestionsRepository implements QuestionsRepository
 {
     private $data_source_path = '../dataSources/questions.json';
-    private $questions = [];
-
-    public function __construct()
-    {
-        $this->load();
-    }
 
     public function getQuestions()
     {
-        return $this->questions;
+        $content = file_get_contents($this->data_source_path);
+
+        return json_decode($content);
     }
 
     public function addQuestion(Question $question)
     {
-        array_push($this->questions, $question);
-    }
-
-    private function load()
-    {
         $content = file_get_contents($this->data_source_path);
-        $decoded_content = json_decode($content);
+        $questions = json_decode($content);
 
-        $this->questions = $decoded_content;
+        array_push($questions, $question);
+
+        $updatedQuestions = json_encode($questions);
+
+        file_put_contents($this->data_source_path, $updatedQuestions);
     }
 }
